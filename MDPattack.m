2a36e1a74d52179b647e7b28d6d209b32d83a09f
sxw13 <sunxw09@126.com>
2014-09-24 10:40:04 +0800

    Multi-attack change

--- MDPattack.m
+++ MDPattack.m
@@ -1,13 +1,5 @@
 %state=1:初始化，state=0:不初始化
-function [ResultData,Config]=MDPattack(commands,label,MDPData)
-
-% global MDPData TAction;
-% global MDPData;
-
-% SSS = load('Action.mat');
-% TAction = SSS.Action;
-
-
+function [ResultData,Config]=MDPattack(Config,label,MDPData)
 addpath([pwd, '\coSimu']);
 addpath([pwd, '\psat']);
 addpath([pwd, '\psat\filters']);
@@ -17,98 +9,10 @@ addpath([pwd, '\debug']);
 addpath([pwd, '\loadshape']);
 pwdpath = pwd;
 
-Config = initialConfig;
-
 if nargin<3
     Config.MDPData = [];
 else
     Config.MDPData = MDPData;
-end
-
-Config.loadShapeCsvFile = 'LoadShape2.csv';
-Config.caseName = 'd_009ieee_edit.m';
-Config.measLagSchema = 1; %1 for perfect comm with no latency; 2 for same latency for all tunnels; 3 for dif. latency for dif. tunnels;
-Config.measAllLatency = 1; % for latency of Config.measAllLatency*Config.DSSStepsize 
-Config.measLatencyChagePeriod = [0, Config.simuEndTime]; 
-Config.measLaggedTunnel = 1 : 1 : 30;
-Config.measTunnelLatency = zeros(size(Config.measLaggedTunnel));
-Config.ctrlLagSchema = 1; %1 for perfect comm with no latency; 2 for same latency for all tunnels; 3 for dif. latency for dif. tunnels;
-Config.ctrlAllLatency = 1; % for latency of Config.ctrlAllLatency
-Config.ctrlTGap = 0.1; % control time within current time +/- ctrlTGap => ctrl operation  
-Config.subAttackSchema = 1; % 1 for no substation attack ; % 2 for substation lost after attacks
-Config.attackedBus = []; % bus list been attacked
-Config.attackTime = [];  % attacked time in seconds
-Config.enableLoadShape = 0;
-Config.distrsw = 0; % 0 for single slack bus model, 1 for distributed slack bus model.
-Config.calEigs = 1; % 1 for calculate the eigent values of the Jaccobi matrix
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%for bad data injection%%%%%%%%%%%%%%%%%%%
-Config.falseDataSchema = 2; % 0 for no false data  ; 1 for random erro based on white noise ; 2 for special false data strategy
-%%%%%%%%%%%%%define a false attack element
-FalseData.toBus = 5;
-FalseData.strategy = 6; % for MDP attack on pl and ql; 
-FalseData.MDPBusVStateStep = 0.01;
-FalseData.MDPStateName = {'ploadMeas(1)'};
-FalseData.MDPStateLimits = [0.7 1.3];
-FalseData.Nstate = [5];  % total number of state
-FalseData.Naction = [5 5 5 5 5];   % total number of action
-FalseData.MDPBusFalseDataRatioStep = [1 1 1 1 0.05];  % Step for false data ratio
-FalseData.PenalForNotConvergence = 1;  % 1 for penal ; 0 for not penal
-FalseData.InjectionName = {'plineHeadMeas(8)','qlineHeadMeas(8)','plineTailMeas(6)','qlineTailMeas(6)','busVMeasPu(5)'};
-FalseData.MDPDiscountFactor = 0;   % discount factor for value function of MDP
-FalseData.RatioOffset = [2 0 2 0 1];
-FalseData.reward = 'voltage';  % 'voltage' or 'pLoss' or 'minEigValue'
-FalseData.Qlearning = 1; % 1 for learning; 0 for not learning
-FalseData.LearningEndTime = 24 * 3600;
-FalseData.learningRate = '2/(sqrt(Iter+1)+1)';
-FalseData.fixedAction = [];  %-1 for a 
-% FalseData.Continouslearning = 1-state; % 0 for setting all state iteration to zero;
-%%%%%%%%%%%%%put a false attack element into config structure
-Config.falseDataAttacks = {FalseData};
-
-%%%%%%%%%%%%%define a false attack element
-FalseData.InjectionName = {'plineHeadMeas(9)','qlineHeadMeas(9)','plineTailMeas(7)','qlineTailMeas(7)','busVMeasPu(6)'};
-Config.falseDataAttacks{length(Config.falseDataAttacks)+1} = FalseData;
-%%%%%%%%%%%%%put a false attack element into config structure
-% 
-%%%%%%%%%%%%%define a false attack element
-FalseData.InjectionName = {'plineTailMeas(5)','qlineTailMeas(5)','plineTailMeas(4)','qlineTailMeas(4)','busVMeasPu(8)'};
-Config.falseDataAttacks{length(Config.falseDataAttacks)+1} = FalseData;
-%%%%%%%%%%%%%put a false attack element into config structure
-
-%%%%%%%%%%%%%define a false attack element
-FalseData.InjectionName = {'plineTailMeas(1)','qlineTailMeas(1)','genPMeas(1)','genQMeas(1)','busVMeasPu(1)'};
-Config.falseDataAttacks{length(Config.falseDataAttacks)+1} = FalseData;
-%%%%%%%%%%%%%put a false attack element into config structure
-
-%%%%%%%%%%%%%define a false attack element
-FalseData.InjectionName = {'plineTailMeas(2)','qlineTailMeas(2)','genPMeas(2)','genQMeas(2)','busVMeasPu(2)'};
-Config.falseDataAttacks{length(Config.falseDataAttacks)+1} = FalseData;
-%%%%%%%%%%%%%put a false attack element into config structure
-
-%%%%%%%%%%%%%define a false attack element
-FalseData.InjectionName = {'plineTailMeas(3)','qlineTailMeas(3)','genPMeas(3)','genQMeas(3)','busVMeasPu(3)'};
-Config.falseDataAttacks{length(Config.falseDataAttacks)+1} = FalseData;
-%%%%%%%%%%%%%put a false attack element into config structure
-
-
-% Config.falseDataAttacks = {FalseData,FalseData2,FalseData3}; % target buses
-
-% if state
-%     MDPData = cell(1,length(Config.falseDataAttacks));
-% end
-
-% enable state estimation
-Config.seEnable = 1;
-
-%Time 
-Config.simuEndTime =  24 * 3600;
-Config.controlPeriod = 60;
-Config.sampleRate  = 10;
-Config.lfTStep = 10;
-
-for k = 1:length(commands)
-    eval([commands{k} ';']);
 end
 
 if Config.simuType == 0
