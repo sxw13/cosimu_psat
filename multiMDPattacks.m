7203f49b3d5c9608f8e6b82e99bf2d49b15bb18a
sxw13 <sunxw09@126.com>
2015-02-04 09:47:51 +0800

    add

--- multiMDPattacks.m
+++ multiMDPattacks.m
@@ -1,7 +1,7 @@
 clear all;
 %% initial path
 % startTime =  strrep(strrep(datestr(now), ':', '-'), ' ', '-');
-startTime = 'IEEE9BussRare';
+startTime = 'IEEE9LineRare';
 if ~exist(['debug\' startTime],'dir')
     mkdir(['debug\' startTime]);
 end
@@ -9,7 +9,7 @@ initialPath;
 pwdpath = pwd;
 
 %% Import Test case
-[Config, MultiRunConfig, cs] = IEEE9BusAttack;
+[Config, MultiRunConfig, cs] = IEEE9LineAttack;
 
 %% Generate test scenarios
 n = length(MultiRunConfig.ConfigValue) ;
---
+++ testCaseScript/IEEE9BusAttack.m
@@ -0,0 +1,51 @@
+function [Config, MultiRunConfig, cs] = IEEE39BusAttack
+Config = initialConfig;
+
+Config.loadShapeCsvFile = 'LoadShape3.csv';
+Config.LoadShapeRatio = 1;
+Config.caseName = 'd_009ieee_edit.m';
+Config.opfCaseName = 'case_ieee9';
+Config.measLagSchema = 1; %1 for perfect comm with no latency; 2 for same latency for all tunnels; 3 for dif. latency for dif. tunnels;
+Config.measAllLatency = 1; % for latency of Config.measAllLatency*Config.DSSStepsize
+Config.measLatencyChagePeriod = [0, Config.simuEndTime];
+Config.measLaggedTunnel = 1 : 1 : 30;
+Config.measTunnelLatency = zeros(size(Config.measLaggedTunnel));
+Config.ctrlLagSchema = 1; %1 for perfect comm with no latency; 2 for same latency for all tunnels; 3 for dif. latency for dif. tunnels;
+Config.ctrlAllLatency = 1; % for latency of Config.ctrlAllLatency
+Config.ctrlTGap = 0.1; % control time within current time +/- ctrlTGap => ctrl operation
+Config.subAttackSchema = 1; % 1 for no substation attack ; % 2 for substation lost after attacks
+Config.attackedBus = []; % bus list been attacked
+Config.attackTime = [];  % attacked time in seconds
+Config.enableLoadShape = 1;
+Config.distrsw = 0; % 0 for single slack bus model, 1 for distributed slack bus model.
+Config.calEigs = 1; % 1 for calculate the eigent values of the Jaccobi matrix
+
+% enable state estimation
+Config.seEnable = 1;
+
+% Time
+Config.simuEndTime =  36 * 3600;
+Config.controlPeriod = 60;
+Config.sampleRate  = 10;
+Config.lfTStep = 10;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%for bad data injection%%%%%%%%%%%%%%%%%%%
+Config.falseDataSchema = 2; % 0 for no false data  ; 1 for random erro based on white noise ; 2 for special false data strategy
+%%%%%%%%%%%%%define a false attack element
+FalseData.toBus = 5;
+FalseData.strategy = 6; % for MDP attack on pl and ql;
+% FalseData = defaultFalseData(Config,FalseData);
+FalseData.maxLearnedAction = 30;
+
+%%%%%%%%%%%%%put a false attack element into config structure
+Config.falseDataAttacks = {FalseData};
+
+
+cs = eval(Config.opfCaseName);
+
+MultiRunConfig.ConfigName = {'LoadShapeRatio','toBus','errorRatio','duplicate'};
+MultiRunConfig.ConfigValue = {[0.5 0.75 1],1:9,linspace(0.5,2,6),1:3};
+% MultiRunConfig.ConfigName = {'LoadShapeRatio','toBus1','toBus2','errorRatio'};
+% MultiRunConfig.ConfigValue = {[0.3 0.45 0.6],1:39,1:39,linspace(0.5,2,6)};
+
+end
