83e97f796a5c0c76a704a7851a88aa2c232147d3
sxw13 <sunxw09@126.com>
2015-03-19 09:21:22 +0800

    修复默认的cmdfilter的问题

--- resultAnalysis/plotStatFigure.m
+++ resultAnalysis/plotStatFigure.m
@@ -4,7 +4,9 @@ for jj = 2:4
     x = zeros(1,1:m-1);
     y = zeros(1,1:m-1);
     for ii = 2:m
-        if ~isempty(strfind(statTable{ii,1},'duplicate'))
+        if ~isempty(strfind(statTable{ii,1},'maxSEIter'))
+            A = sscanf(statTable{ii,1},'Branch_%d_errorRatio_%f_maxSEIter_%d_.mat');
+        elseif ~isempty(strfind(statTable{ii,1},'duplicate'))
 %         A = sscanf(statTable{ii,1},'LoadShapeRatio_%f_Branch_%d_errorRatio_%f_.mat');
 %         A = sscanf(statTable{ii,1},'LoadShapeRatio_%f_toBus_%d_errorRatio_%f_.mat');
             A = sscanf(statTable{ii,1},'LoadShapeRatio_%f_toBus_%d_errorRatio_%f_duplicate_%d_.mat');
