21cc9c2ec10b33f700dd7176d67820763008bb2d
sxw13 <sunxw09@126.com>
2015-05-05 00:31:47 +0800

    111

--- testAgain.m
+++ testAgain.m
@@ -1,4 +1,4 @@
-load('C:\Users\sxw13\Documents\cosimu_psat\debug\IEEE39BusAttackNew(dynamicPowerLimitation)\LoadShapeRatio_2_seEnable_1_toBus_38_errorRatio_2_.mat');
+load('C:\Users\sxw13\Documents\cosimu_psat\debug\IEEE39BusAttack(dynamicPowerLimitation)\LoadShapeRatio_2.3_toBus_38_errorRatio_2_.mat');
 pwdpath = pwd;
 initialPath;
 if Config.simuType == 0
@@ -11,7 +11,14 @@ delete *.mat
 createhourloadshape(Config);
 
 cd(pwdpath);
+inj = [];
+dct = [];
+fd = fieldnames(ResultData.falseDataDctSet);
+for fid = 1:length(fd)
+    inj = [inj;ResultData.falseDataInjSet.(fd{fid})];
+    dct = [dct;ResultData.falseDataDctSet.(fd{fid})];
+end
 Config.falseDataAttacks{1,1}.fixedAction = ResultData.MDPData{1,1}.ActionHistory;
-
+steals = full(sum(inj & (~dct))).*ResultData.isOpfConverged;
 (find(steals>0)-1)*Config.controlPeriod
 simplePSAT(Config);
